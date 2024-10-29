864f558fc317b2bbb1897b8705df2083aa37d0c0
cevsouza <37272064+cevsouza@users.noreply.github.com>
2024-10-29 09:09:44 -0300

    Update 1-variaveis.py

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,11 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+nome = "Cadu"
 # Declare variáveis de cada tipo numérico
-
+a = 1
+b = 2
+c = 3
 
 # Declare uma variável do tipo booleano
+verdadeiro = True
