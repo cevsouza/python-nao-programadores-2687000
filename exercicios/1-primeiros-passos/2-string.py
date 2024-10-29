cdfd28a9bec7c592d51aa68465a3b1b9fca3da9f
cevsouza <37272064+cevsouza@users.noreply.github.com>
2024-10-29 09:24:40 -0300

    Update 2-string.py

--- exercicios/1-primeiros-passos/2-string.py
+++ exercicios/1-primeiros-passos/2-string.py
@@ -1,7 +1,7 @@
 resumo = "Paloma é uma mulher de 46 anos que deseja mudar de profissão, por isso está estudando 'python'."
 
 # Imprima na tela a variável "resumo"
-
+print resumo
 
 # Imprima na tela apenas a segunda letra da variável
 
